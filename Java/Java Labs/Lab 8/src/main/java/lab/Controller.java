d96e909430fc761e98090ce1d707e71e0c1ffdf6
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-04-01 16:02:04 -0400

    lab 8

--- Java/Java Labs/Lab 8/src/main/java/lab/Controller.java
+++ Java/Java Labs/Lab 8/src/main/java/lab/Controller.java
@@ -1,36 +1,45 @@
 package lab;
 
-/**
- * Name: 
- * Username: 
- */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 
 public class Controller {
 
+    @FXML
+    private MenuItem extra;
+
+    @FXML
+    private Label instructions;
+
+    @FXML
+    private VBox main;
+
+    @FXML
+    private MenuItem normal;
+
+    @FXML
+    private Button play;
+
+    @FXML
+    private Label youWin;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void startGame(ActionEvent event) {
+        youWin.setVisible(false);
+        play.setVisible(false);
+        instructions.setVisible(false);
+
+        for (int i = 0; i < 7; i++) {
+            main.getChildren().add(new RandomCircle(main));
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Java/Java Labs/Lab 8/src/main/java/lab/RandomCircle.java
+++ Java/Java Labs/Lab 8/src/main/java/lab/RandomCircle.java
@@ -1,9 +1,39 @@
 package lab;
 
+import javafx.animation.TranslateTransition;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomCircle extends Circle {
+    private final TranslateTransition translateToCenter = new TranslateTransition(new Duration(3000), this);
     private boolean captured = false;
 
-    public RandomCircle() {}
+    public RandomCircle(Pane pane) {
+        super(ThreadLocalRandom.current().nextInt((int) pane.getWidth()),
+                ThreadLocalRandom.current().nextInt((int) pane.getHeight()),
+                ThreadLocalRandom.current().nextInt(100),
+                new Color[]{Color.GREEN, Color.RED, Color.BLUE}[ThreadLocalRandom.current().nextInt(new Color[]{Color.GREEN, Color.RED, Color.BLUE}.length)]);
+
+        double centerX = pane.getWidth() / 2;
+        double centerY = pane.getHeight() / 2;
+
+        TranslateTransition translateToRandomPoint = new TranslateTransition(new Duration(3000), this);
+        translateToRandomPoint.setToX(ThreadLocalRandom.current().nextInt(0, (int) pane.getWidth()));
+        translateToRandomPoint.setToY(ThreadLocalRandom.current().nextInt(0, (int) pane.getHeight()));
+
+        translateToCenter.setToX(centerX);
+        translateToCenter.setToY(centerY);
+
+        setOnKeyPressed(keyEvent -> {
+            captured = true;
+            translateToCenter.play();
+        });
+
+        translateToRandomPoint.setAutoReverse(true);
+        translateToRandomPoint.play();
+    }
 }
